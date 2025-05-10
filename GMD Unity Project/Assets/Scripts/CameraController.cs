4377aa80c372b43cb3a1103380d0735548f94443
KaBence <kabaly.bence@gmail.com>
2025-05-10 16:06:08 +0200

    camera and player movement is set up correctly

--- GMD Unity Project/Assets/Scripts/CameraController.cs
+++ GMD Unity Project/Assets/Scripts/CameraController.cs
@@ -3,20 +3,36 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private GameObject player;
-    private Vector3 offset;
+    private Camera cam;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float verticalMargin = 0.05f;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        offset = transform.position - player.transform.position;
+        cam = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        // Move the camera forward
+        transform.position += Vector3.forward * speed * Time.deltaTime;
+
+        // Convert player world position to viewport space (0 to 1)
+        Vector3 viewPos = cam.WorldToViewportPoint(player.transform.position);
+
+        // If the player is outside the margins, nudge them back in
+        if (viewPos.y < verticalMargin)
+            viewPos.y = verticalMargin;
+        else if (viewPos.y > 1f - verticalMargin)
+            viewPos.y = 1f - verticalMargin;
+
+        // Convert back to world space
+        Vector3 correctedWorldPos = cam.ViewportToWorldPoint(viewPos);
+
+        // Keep player's Y position (so jumping etc. still works)
+        correctedWorldPos.y = player.transform.position.y;
+
+        player.transform.position = correctedWorldPos;
     }
-
-
 }
--- GMD Unity Project/Assets/Scripts/PlayerMovement.cs
+++ GMD Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -32,36 +33,27 @@ public class PlayerMovement : MonoBehaviour
             Quaternion toRotation = Quaternion.LookRotation(move, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 720 * Time.deltaTime);
         }
-
-        var keyboard = Keyboard.current;
-        var gamepad = Gamepad.current;
-
-        bool isHeld = (keyboard != null && keyboard.leftShiftKey.isPressed) ||
-                      (gamepad != null && gamepad.leftTrigger.ReadValue() > 0.1f);
-
-        animator.SetBool(isRunningHash, isHeld);
-        speed = isHeld ? 10 : 5;
+    
     }
 
-    public void OnMovement(InputValue value)
+    public void OnMovement(InputAction.CallbackContext value)
     {
-        movement = value.Get<Vector2>();
+        movement = value.ReadValue<Vector2>();
         animator.SetBool(isWalkingHash, movement != Vector2.zero);
-
     }
 
-    public void OnLeftTrigger(InputValue value)
+    public void OnLeftTrigger(InputAction.CallbackContext value)
     {
         // Debug.Log("Left Trigger: " + value.isPressed);
-        // if (value.isPressed)
-        // {
-        //     animator.SetBool(isRunningHash, true);
-        //     speed = 10;
-        // }
-        // else
-        // {
-        //     animator.SetBool(isRunningHash, false);
-        //     speed = 5;
-        // }
+        if (value.phase == InputActionPhase.Started)
+        {
+            animator.SetBool(isRunningHash, true);
+            speed = 10;
+        }
+        else if (value.phase == InputActionPhase.Canceled)
+        {
+            animator.SetBool(isRunningHash, false);
+            speed = 5;
+        }
     }
 }
